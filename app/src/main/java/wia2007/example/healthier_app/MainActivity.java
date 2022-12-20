ec28b9173c6d985371debe16615a8b305b6558d5
derrick phang <derrickphb@gmail.com>
2022-12-20 09:10:57 +0800

    This is the base of the project files

---
+++ app/src/main/java/wia2007/example/healthier_app/MainActivity.java
@@ -0,0 +1,14 @@
+package wia2007.example.healthier_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
