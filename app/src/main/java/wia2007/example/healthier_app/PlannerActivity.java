8e78f588477430d6da484c9d5aa8bf059008ddde
derrick phang <derrickphb@gmail.com>
2022-12-26 17:41:25 +0800

    Phang 26 Dec 2022: Working on Diet Planner fragment UI design. Part 5
    Remove action/tool bar for every activity, set custom tool bar in diet planner.

--- app/src/main/java/wia2007/example/healthier_app/PlannerActivity.java
+++ app/src/main/java/wia2007/example/healthier_app/PlannerActivity.java
@@ -1,8 +1,13 @@
 package wia2007.example.healthier_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class PlannerActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class PlannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+    }
+
+    public void BtnCompleteOnClick(View view) {
+        String message = "'Complete' button pressed";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    public void BtnEditOnClick(View view) {
+        String message = "'Edit' button pressed";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
