3e0ff223036f5dbc3b69ef735a5a2a25a7c713db
derrick phang <derrickphb@gmail.com>
2022-12-30 10:27:03 +0800

    Phang 30 Dec 2022: Fixed Alvin's issue. Cannot resolve font family issue, runtime won't show the font style, please revise your method.
    WIP on bottom navigation bar

--- app/src/main/java/wia2007/example/healthier_app/MainActivity.java
+++ app/src/main/java/wia2007/example/healthier_app/MainActivity.java
@@ -1,10 +1,18 @@
 package wia2007.example.healthier_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.FCVBlank);
+        NavController navController = host.getNavController();
+        setupBottomNavMenu(navController);
     }
 
     public void BtnPlannerOnClick(View view) {
@@ -23,4 +35,26 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), CalculatorActivity.class);
         startActivity(intent);
     }
+
+    private void setupBottomNavMenu(NavController navController) {
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bottom, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        try {
+            Navigation.findNavController(this, R.id.FCVBlank).navigate(item.getItemId());
+            return true;
+        } catch (Exception ex) {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
