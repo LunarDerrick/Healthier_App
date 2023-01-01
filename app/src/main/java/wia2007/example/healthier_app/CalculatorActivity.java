67fbda5498cf2ae31b8a78d25a6af09f964ac516
derrick phang <derrickphb@gmail.com>
2023-01-01 16:01:52 +0800

    Phang 1 Jan 2023: Working on Fasting Planner Part 2 (The timer circle needs to be revised) Planner module UI done
    Added temp button for Calculator Activity to navigate btw fragments

--- app/src/main/java/wia2007/example/healthier_app/CalculatorActivity.java
+++ app/src/main/java/wia2007/example/healthier_app/CalculatorActivity.java
@@ -1,8 +1,10 @@
 package wia2007.example.healthier_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void BtnBMIMacroOnClick(View view) {
+        FragmentContainerView FCVBMIMacro = findViewById(R.id.FCVBMIMacro);
+        FragmentContainerView FCVCalorie = findViewById(R.id.FCVCalorie);
+        FCVBMIMacro.setVisibility(View.VISIBLE);
+        FCVCalorie.setVisibility(View.GONE);
+    }
+
+    public void BtnCalorieOnClick(View view) {
+        FragmentContainerView FCVBMIMacro = findViewById(R.id.FCVBMIMacro);
+        FragmentContainerView FCVCalorie = findViewById(R.id.FCVCalorie);
+        FCVBMIMacro.setVisibility(View.GONE);
+        FCVCalorie.setVisibility(View.VISIBLE);
     }
 }
