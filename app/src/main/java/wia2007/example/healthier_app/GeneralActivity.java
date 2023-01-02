b17fa611e1fe2fa0b3df290f8950aa8650286744
derrick phang <derrickphb@gmail.com>
2023-01-02 14:31:37 +0800

    Phang 2 Jan 2023: Re-adjust navigation. Now activity upon launch should be GeneralActivity, so first view is login page. Blue text can navigate between login & register.
    [!]If run into error, left side of debug "app" button with android icon, edit configuration:
    - launch options: specified activity
    - GeneralActivity then apply, should work

--- app/src/main/java/wia2007/example/healthier_app/GeneralActivity.java
+++ app/src/main/java/wia2007/example/healthier_app/GeneralActivity.java
@@ -1,8 +1,12 @@
 package wia2007.example.healthier_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class GeneralActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class GeneralActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
+    }
+
+    public void BtnNoAccOnClick(View view) {
+        FragmentContainerView FCVRegister = findViewById(R.id.FCVRegister);
+        FragmentContainerView FCVLogin = findViewById(R.id.FCVLogin);
+        FCVRegister.setVisibility(View.VISIBLE);
+        FCVLogin.setVisibility(View.GONE);
+    }
+
+    public void BtnHasAccOnClick(View view) {
+        FragmentContainerView FCVRegister = findViewById(R.id.FCVRegister);
+        FragmentContainerView FCVLogin = findViewById(R.id.FCVLogin);
+        FCVRegister.setVisibility(View.GONE);
+        FCVLogin.setVisibility(View.VISIBLE);
+    }
+
+    public void BtnLoginOnClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
