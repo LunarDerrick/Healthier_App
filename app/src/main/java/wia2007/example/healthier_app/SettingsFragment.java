87abf13fc4e6edfe5936c9ca4384dae367233153
derrick phang <derrickphb@gmail.com>
2023-01-10 23:11:39 +0800

    Phang 10 Jan 2023: readd imports because I overwritten them

--- app/src/main/java/wia2007/example/healthier_app/SettingsFragment.java
+++ app/src/main/java/wia2007/example/healthier_app/SettingsFragment.java
@@ -1,11 +1,12 @@
 package wia2007.example.healthier_app;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsFragment extends Fragment {
 
